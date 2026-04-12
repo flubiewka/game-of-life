2413ca68ac79479bd9951760d4cf08f6b1d3d664
flubiewka <flubiewka@gmail.com>
2026-04-13 01:12:41 +0200

    comms

--- src/config.py
+++ src/config.py
@@ -10,9 +10,9 @@ GAPI_COLORS = {"VOID": "#13a10e", "ALIVE": "#c50f1f", "WAS_ALIVE": "#00af00"}
 
 ANSI = {
     "RESET": "\033[0m",
-    "B_RED": "\033[41m",
-    "B_GREEN": "\033[42m",
-    "B_DARK_GREEN": "\033[48;5;34m",
+    "B_RED": "\033[41m",  #! alive
+    "B_GREEN": "\033[42m",  #! void
+    "B_DARK_GREEN": "\033[48;5;34m",  #!was alive
     "CLEAR_SCREEN": "\033[H\033[J",
     "CURSOR_HOME": "\033[H",
     "HIDE_CURSOR": "\033[?25l",
